830d7b0cdaec27f8149a7cd90c8d028ef46e5dfb
Анастасия <shafirova.a.d@yandex.ru>
2024-04-29 23:36:10 +0400

    Добавили еще один вариант решения задачи, более правильный

---
+++ Lec2/Lec2_Task3/Program.cs
@@ -0,0 +1,26 @@
+﻿// int n = 10;
+// int[] arr = new int[n];
+// int i = 0;
+
+// while (i < arr.Length)
+// {
+//     arr[i] = i + 2;
+//     // Console.Write(arr[i]);
+//     // Console.Write(' ');
+//     Console.Write($"{arr[i]} ");
+
+//     i = i + 2;
+// }
+
+int n = 10;
+int[] array = {2,5,4,7,8,35,1,3,8,5};
+int i = 0;
+
+while (i < n)
+{
+    if(array[i] % 2 == 0)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    i = i + 1;
+}
