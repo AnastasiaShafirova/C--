54a0c933e14fa91d44129a873283f9e06b1d1a19
Анастасия <shafirova.a.d@yandex.ru>
2024-04-30 00:12:47 +0400

    Добавили несколько решений задачи

--- Lec2/Lec2_Task4/Program.cs
+++ Lec2/Lec2_Task4/Program.cs
@@ -1,13 +1,41 @@
 ﻿int n = 5;
 int[] array = { 7, 4, 15, 3, 9 };
-int i = 0;
+
+
+// int i = 0;
+// int max = array[0];
+
+// while (i < n)
+// {
+//     if (array[i] > max)
+//     {
+//         max = array[i];
+//     }
+//     i = i + 1;
+// }
+// Console.WriteLine(max);
+
+
+
 int max = array[0];
-while(i < n)
+for (int j = 0; j < n; j++)
 {
-    if (array[i] > max)
+    if (array[j] > max)
     {
-        max = array[i];
+        max = array[j];
     }
-    i = i + 1;
+}
+Console.WriteLine(max);
+
+
+
+
+max = array[0];
+foreach (int e in array)
+{
+    if (e > max)
+    {
+        max = e;
+    }
 }
 Console.WriteLine(max);
