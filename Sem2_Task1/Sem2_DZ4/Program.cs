60c8c09a5684cdafee4378c692f472922f4325fa
Анастасия <shafirova.a.d@yandex.ru>
2024-04-29 19:55:12 +0400

    Решили задачу 4

---
+++ Sem2_Task1/Sem2_DZ4/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая на вход принимает натуральное число N, 
+// а на выходе показывает его цифры через запятую.
+
+Console.WriteLine("Введите натуральное число ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 10)
+{
+    Console.WriteLine(num);
+}
+else
+{
+    while (num > 0)
+    {
+        int currentDigit = num % 10;
+        num /= 10;
+        if (num > 0)
+        {
+            Console.WriteLine(currentDigit + ",");
+        }
+        else
+        {
+            Console.WriteLine(currentDigit);
+        }
+    }
+}
+
