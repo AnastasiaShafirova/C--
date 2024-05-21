2d976a341984719c576b251a01aabffdc518f551
Анастасия <shafirova.a.d@yandex.ru>
2024-05-22 00:38:03 +0400

    Решили задачу 2-мерные массивы и вывели только буквы на экран

---
+++ Lec2/Lec4_Task3/Program.cs
@@ -0,0 +1,15 @@
+﻿string GetLettersFromStr(string s)
+{
+    string letters = "";
+    foreach(char e in s)
+    {
+        if(char.IsAsciiLetter(e) == true)
+        {
+            letters = letters + e;
+        }
+    }
+    return letters;
+}
+string str = Console.ReadLine();
+string resalt = GetLettersFromStr(str);
+Console.WriteLine(resalt);
